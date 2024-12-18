14765a41bac77cac046430d444c91688b1201435
anhcraft <huynhduyanh123123@gmail.com>
2024-12-18 18:41:07 +0700

    Fix AuraSkills handle to custom drops

--- plugin/src/main/java/dev/anhcraft/oreprocessor/handler/ProcessingPlant.java
+++ plugin/src/main/java/dev/anhcraft/oreprocessor/handler/ProcessingPlant.java
@@ -1,5 +1,6 @@
 package dev.anhcraft.oreprocessor.handler;
 
+import com.google.common.base.Preconditions;
 import dev.anhcraft.oreprocessor.OreProcessor;
 import dev.anhcraft.oreprocessor.api.Ore;
 import dev.anhcraft.oreprocessor.api.data.OreData;
@@ -20,6 +21,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -116,6 +118,7 @@ public final class ProcessingPlant {
 
     PlayerData playerData = OreProcessor.getApi().getPlayerData(player);
     OreData oreData = playerData.requireOreData(ore.getId());
+    // early check for optimization
     if (oreData.isFull() && plugin.mainConfig.behaviourSettings.dropOnFullStorage)
       return Result.PASS;
 
@@ -135,6 +138,10 @@ public final class ProcessingPlant {
         int amount = itemStack.getAmount();
 
         // TODO there is currently no event fired for picking up (through the collector)
+
+        // re-check every time we add a feedstock
+        if (oreData.isFull() && plugin.mainConfig.behaviourSettings.dropOnFullStorage)
+          break; // for remaining, the item entities are not removed, so they would be dropped later
 
         if (ore.isAcceptableFeedstock(feedstock)) {
           StatisticHelper.increaseFeedstockCount(ore.getId(), amount, playerData);
@@ -156,7 +163,8 @@ public final class ProcessingPlant {
   }
 
   /**
-   * Collects the given loot directly.
+   * Collects the given loot directly.<br>
+   * Any item that is successfully collected would be removed from the given list
    * Cautions:
    * <ul>
    *   <li>The call must be made <b>synchronous</b></li>
@@ -169,6 +177,8 @@ public final class ProcessingPlant {
    * @return result
    */
   public Result collectLoot(Player player, BlockState state, List<Item> loot) {
+    Preconditions.checkArgument(!isUnmodifiable(loot), "Given loot must be modifiable");
+
     if (!isEnabled(player))
       return Result.PASS;
 
@@ -210,6 +220,75 @@ public final class ProcessingPlant {
     }
 
     return Result.PASS_WITH_IN_PLACE_MODIFY;
+  }
+
+  /**
+   * Collects the given loot directly.<br>
+   * Any item that is successfully collected would be removed from the given list
+   * Cautions:
+   * <ul>
+   *   <li>The call must be made <b>synchronous</b></li>
+   *   <li>Multiple calls could result in duplication</li>
+   * </ul>
+   * @param player the player
+   * @param loot the loot (must be modifiable)
+   * @return result
+   */
+  public Result collectLoot(Player player, Collection<? extends ItemStack> loot) {
+    Preconditions.checkArgument(!isUnmodifiable(loot), "Given loot must be modifiable");
+
+    if (!isEnabled(player))
+      return Result.PASS;
+
+    PlayerData playerData = plugin.playerDataManager.getData(player);
+    boolean has = false;
+
+    for (Iterator<? extends ItemStack> it = loot.iterator(); it.hasNext(); ) {
+      ItemStack item = it.next();
+      UMaterial feedstock = OreProcessor.getApi().identifyMaterial(item);
+      if (feedstock == null) continue;
+      int amount = item.getAmount();
+      Collection<Ore> ores = OreProcessor.getApi().getOresAllowFeedstock(feedstock);
+      if (ores.isEmpty()) continue;
+
+      boolean added = false;
+
+      // TODO we should implement a dynamic storing similar to /ore store
+      for (Ore ore : ores) {
+        OreData oreData = playerData.requireOreData(ore.getId());
+        if (oreData.isFull() && plugin.mainConfig.behaviourSettings.dropOnFullStorage)
+          continue;
+
+        added = true;
+        StatisticHelper.increaseFeedstockCount(ore.getId(), amount, playerData);
+        StatisticHelper.increaseFeedstockCount(ore.getId(), amount, OreProcessor.getApi().getServerData());
+        oreData.addFeedstock(feedstock, amount);
+        it.remove();
+        break; // add once only
+      }
+
+      if (added) {
+        it.remove();
+        has = true;
+      }
+    }
+
+    if (has && !playerData.isTutorialHidden()) {
+      for (String msg : plugin.messageConfig.firstTimeTutorial) {
+        plugin.rawMsg(player, msg);
+      }
+    }
+
+    return Result.PASS_WITH_IN_PLACE_MODIFY;
+  }
+
+  private static boolean isUnmodifiable(Collection<?> collection) {
+    try {
+      collection.addAll(Collections.emptyList());
+      return false;
+    } catch (UnsupportedOperationException UnsupportedOperationException) {
+      return true;
+    }
   }
 
   private boolean isEnabled(Player player) {
--- plugin/src/main/java/dev/anhcraft/oreprocessor/integration/AuraSkillsBridge.java
+++ plugin/src/main/java/dev/anhcraft/oreprocessor/integration/AuraSkillsBridge.java
@@ -1,21 +1,17 @@
 package dev.anhcraft.oreprocessor.integration;
 
 import dev.anhcraft.oreprocessor.OreProcessor;
-import dev.anhcraft.oreprocessor.api.Ore;
-import dev.anhcraft.oreprocessor.api.data.OreData;
-import dev.anhcraft.oreprocessor.api.data.PlayerData;
-import dev.anhcraft.oreprocessor.api.util.UMaterial;
-import dev.anhcraft.oreprocessor.storage.stats.StatisticHelper;
 import dev.aurelium.auraskills.api.event.loot.LootDropEvent;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public class AuraSkillsBridge implements Integration, Listener, EventDebugger {
@@ -26,36 +22,16 @@ public class AuraSkillsBridge implements Integration, Listener, EventDebugger {
     plugin.getServer().getPluginManager().registerEvents(this, plugin);
   }
 
-  @EventHandler(ignoreCancelled = true)
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
   private void onDropLoot(LootDropEvent event) {
     Player player = event.getPlayer();
-    ItemStack item = event.getItem();
-    UMaterial feedstock = OreProcessor.getApi().identifyMaterial(item);
-    if (feedstock == null) return;
-    int amount = item.getAmount();
-    Collection<Ore> ores = OreProcessor.getApi().getOresAllowFeedstock(feedstock);
-    if (ores.isEmpty()) return;
 
-    PlayerData playerData = plugin.playerDataManager.getData(player);
-    boolean isFull = false;
+    // We would try to collect the loot, if one is collected, it would be removed from the list
+    List<ItemStack> itemStacks = new ArrayList<>(1);
+    itemStacks.add(event.getItem());
+    plugin.processingPlant.collectLoot(player, itemStacks);
 
-    for (Ore ore : ores) {
-      OreData oreData = playerData.requireOreData(ore.getId());
-      if (oreData.isFull()) {
-        isFull = true;
-        continue;
-      }
-
-      isFull = false;
-      StatisticHelper.increaseFeedstockCount(ore.getId(), amount, playerData);
-      StatisticHelper.increaseFeedstockCount(ore.getId(), amount, OreProcessor.getApi().getServerData());
-      oreData.addFeedstock(feedstock, amount);
-      event.setCancelled(true);
-      event.setItem(new ItemStack(Material.AIR));
-      break; // add once only
-    }
-
-    if (isFull && !plugin.mainConfig.behaviourSettings.dropOnFullStorage)
+    if (itemStacks.isEmpty())
       event.setCancelled(true);
   }
 
