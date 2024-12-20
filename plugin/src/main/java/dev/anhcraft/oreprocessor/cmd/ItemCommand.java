e1088c010278dcf4843bc6d064a80a78fbf5e7c7
anhcraft <huynhduyanh123123@gmail.com>
2024-12-20 19:20:14 +0700

    Change give-item command

--- plugin/src/main/java/dev/anhcraft/oreprocessor/cmd/ItemCommand.java
+++ plugin/src/main/java/dev/anhcraft/oreprocessor/cmd/ItemCommand.java
@@ -3,6 +3,7 @@ package dev.anhcraft.oreprocessor.cmd;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.*;
+import co.aikar.commands.bukkit.contexts.OnlinePlayer;
 import dev.anhcraft.oreprocessor.OreProcessor;
 import dev.anhcraft.oreprocessor.api.util.UMaterial;
 import dev.anhcraft.palette.util.ItemUtil;
@@ -28,8 +29,18 @@ public class ItemCommand extends BaseCommand {
     @Subcommand("item give")
     @CommandPermission("oreprocessor.item.give")
     @Description("Give an item to the inventory (or drop if full)")
-    @CommandCompletion("@players @materials")
-    public void giveItem(CommandSender sender, Player player, String material, int amount) {
+    @CommandCompletion("@materials @range:1-5 @players")
+    public void giveItem(CommandSender sender, String material, int amount, @Optional OnlinePlayer player) {
+        Player target = player != null ? player.getPlayer() : null;
+        if (target == null) {
+            if (sender instanceof Player)
+                target = (Player) sender;
+            else {
+                sender.sendMessage(ChatColor.RED + "You must specify a player or you must be in-game!");
+                return;
+            }
+        }
+
         UMaterial uMaterial = UMaterial.parse(material);
         if (uMaterial == null) {
             sender.sendMessage(ChatColor.RED + "Invalid material!");
@@ -39,6 +50,6 @@ public class ItemCommand extends BaseCommand {
         ItemStack item = OreProcessor.getApi().buildItem(uMaterial, amount);
         if (item == null || ItemUtil.isEmpty(item)) return;
 
-        ItemUtil.addToInventory(player, item);
+        ItemUtil.addToInventory(target, item);
     }
 }
